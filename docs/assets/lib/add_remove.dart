507d67b3296dd1b1a881a375a9d613df5f71a67a
Carlos Eduardo <cadu@caduandrade.net>
2022-01-26 16:33:33 -0300

    Updating

--- docs/assets/lib/add_remove.dart
+++ docs/assets/lib/add_remove.dart
@@ -1,16 +1,21 @@
 import 'dart:math' as math;
+import 'package:demoflu/demoflu.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:multi_split_view/multi_split_view.dart';
 import 'package:multi_split_view_demo/example_widget.dart';
 
-class AddRemoveExample extends StatefulWidget {
+class AddRemoveExample extends Example {
   @override
-  State<StatefulWidget> createState() => AddRemoveExampleState();
+  Widget buildMainWidget(BuildContext context) => MainWidget();
 }
 
-class AddRemoveExampleState extends State<AddRemoveExample>
-    with ContentBuilder {
+class MainWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MainWidgetState();
+}
+
+class MainWidgetState extends State<MainWidget> with ContentBuilder {
   MultiSplitViewController _controller = MultiSplitViewController();
   int _count = 3;
 
--- docs/assets/lib/listener.dart
+++ docs/assets/lib/listener.dart
@@ -3,7 +3,12 @@ import 'package:flutter/widgets.dart';
 import 'package:multi_split_view/multi_split_view.dart';
 import 'package:multi_split_view_demo/example_widget.dart';
 
-class SizeListenerExample extends StatelessWidget with ContentBuilder {
+class SizeListenerExample extends Example {
+  @override
+  Widget buildMainWidget(BuildContext context) => MainWidget();
+}
+
+class MainWidget extends StatelessWidget with ContentBuilder {
   @override
   Widget build(BuildContext context) {
     Widget child1 = buildContent(1);
