745ce7e5e66b04df245665c9d333261f4b04e3e5
Carlos Eduardo <cadu@caduandrade.net>
2022-01-26 16:20:19 -0300

    Updating dependencies

--- lib/divider_color.dart
+++ lib/divider_color.dart
@@ -1,6 +1,5 @@
 import 'package:demoflu/demoflu.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:multi_split_view/multi_split_view.dart';
 import 'package:multi_split_view_demo/example_widget.dart';
 
